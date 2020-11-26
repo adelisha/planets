ea6667030f7724d0216062d8f59ca0841f44e9a8
Adelina Khayrullina <65246538+adelisha@users.noreply.github.com>
2020-11-26 20:45:14 +0500

    удаление записей

---
+++ delete.php
@@ -0,0 +1,8 @@
+﻿<?php
+$connect = mysqli_connect("localhost", "root", "root", "planets" );
+mysqli_query($connect, "SET NAMES utf8");
+$zapros1="DELETE FROM center WHERE id=".$_GET['id'];
+mysqli_query($connect, $zapros1);
+echo "<script>window.location.href='lab4.php'</script>";
+exit;
+?>
